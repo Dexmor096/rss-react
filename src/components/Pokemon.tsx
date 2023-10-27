d427277935cab33a1a86c55caec978cef6a0f702
Dexter096 <gr1nch2@yandex.ru>
2023-10-27 21:20:15 +0300

    feat: add pokemonlist and pokemon templates

---
+++ src/components/Pokemon.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Pokemon extends React.Component {
+	constructor(props: string){
+		super(props)
+	}
+		render() {
+			return (
+				<span>This is a Pokemon</span>
+			)
+		}
+}
