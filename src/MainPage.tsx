9e78e4f01c4a7fe7543232219803a2f65d600fc8
Dexter096 <gr1nch2@yandex.ru>
2023-10-28 13:20:37 +0300

    feat: add not-found page

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
+import NotFound from './components/404'
 import ItemList from './components/ItemList';
 import Search from './components/Search';
 
 export default class MainPage extends React.Component {
 	state = {
-		items: []
+		items: [],
+		isLoading: false
 	};
 
 	loadPokemon = (string : string) => {
+		this.setState({isLoading: true})
 		fetch(`https://rickandmortyapi.com/api/character/?name=${string}`)
 		.then(response => response.json())
-		.then(data => this.setState({items: data.results}))
+		.then(data => this.setState({items: data.results, isLoading: false}))
 		.catch(Error)
 	}
  	render() {
 		return(
 			<>
 				<Search loadPokemon={this.loadPokemon}/>
+				{ this.state.items.length < 1 ? (<NotFound />) : 
 				<ItemList items={this.state.items}/>
+				}
 			</>
 		)
  }
---
+++ src/components/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class NotFound extends React.Component {
+	render() {
+		return (
+		<>
+			<h2>Not found</h2>
+			<img src={'../assets/404.jpg'} alt="Not found" />
+		</>
+		)
+	}
+}
